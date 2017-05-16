4f3837ccf2113fda7b386771b0de6acae587ea52
xurong <547217475@qq.com>
2017-05-16 10:40:27 +0800

    修复L3Demo黑屏的bug

--- L3Demo/app/src/main/java/sunmi/l3demo/ResultReceiver.java
+++ L3Demo/app/src/main/java/sunmi/l3demo/ResultReceiver.java
@@ -3,6 +3,7 @@ package sunmi.l3demo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.util.Log;
 
 /**
@@ -14,10 +15,10 @@ public class ResultReceiver extends BroadcastReceiver {
     private static final String TAG = "ResultReceiver";
 
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, Intent intent) {
         String action = intent.getAction();
-        if (action.equals("sunmi.payment.L3Route.RESULT")) {
-            int resultCode = intent.getIntExtra("resultCode", -1);
+        if (action.equals("sunmi.payment.L3.RESULT")) {
+            final int resultCode = intent.getIntExtra("resultCode", -1);
             long amount = intent.getLongExtra("amount", 0);
             // 原交易凭证号
             String voucherNo = intent.getStringExtra("voucherNo");
@@ -41,7 +42,7 @@ public class ResultReceiver extends BroadcastReceiver {
             int transNum = intent.getIntExtra("transNum", 0);
             long totalAmount = intent.getLongExtra("totalAmount", 0L);
 
-            String resultInfo = resultCode + "\namount:" + amount + "\nvoucherNo:" + voucherNo
+            final String resultInfo = resultCode + "\namount:" + amount + "\nvoucherNo:" + voucherNo
                     + "\nreferenceNo:" + referenceNo + "\nbatchNo:" + batchNo + "\ncardNo:" + cardNo + "\ncardType:"
                     + cardType + "\nissue:" + issue + "\nterminalId:" + terminalId + "\nmerchantId:" + merchantId
                     + "\nmerchantName:" + merchantName + "\npaymentType:" + paymentType + "\ntransDate:" + date
@@ -50,12 +51,18 @@ public class ResultReceiver extends BroadcastReceiver {
                     + "\ntransNum:" + transNum + "\ntotalAmount:" + totalAmount;
 
             Log.e(TAG, resultInfo);
-            Intent myIntent = new Intent(context, ResultActivity.class);
-            myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            myIntent.putExtra("resultCode", resultCode);
-            myIntent.putExtra("resultInfo", resultInfo);
-            myIntent.putExtra("errorMsg", errorMsg);
-            context.startActivity(myIntent);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent myIntent = new Intent(context, ResultActivity.class);
+                    myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    myIntent.putExtra("resultCode", resultCode);
+                    myIntent.putExtra("resultInfo", resultInfo);
+                    myIntent.putExtra("errorMsg", errorMsg);
+                    context.startActivity(myIntent);
+                }
+            }, 500);
+
         }
     }
 }
