de0d35b308394ed2d1401d86ec6e54b1dfd735d2
nishenzhou <nishenzhou@sunmi.com>
2019-07-09 11:54:48 +0800

    修改 结果页面的一些逻辑

--- L3Demo/app/src/main/java/com/sm/l3/demo/ResultActivity.java
+++ L3Demo/app/src/main/java/com/sm/l3/demo/ResultActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.util.Set;
+
 public class ResultActivity extends AppCompatActivity {
 
     private static final String TAG = "ResultActivity";
@@ -13,70 +15,38 @@ public class ResultActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_result);
 
         TextView tvResult = findViewById(R.id.tv_result);
 
         Intent intent = getIntent();
+
         StringBuilder sb = new StringBuilder();
 
-        int code = intent.getIntExtra("resultCode", -99999);
-        if (code == 0) {
-            sb.append("交易成功, 具体信息请查看控制台的Log \n");
-        } else {
-            sb.append("交易失败, 具体信息请查看控制台的Log \n");
+        try {
+            Set<String> keySet = intent.getExtras().keySet();
+            for (String key : keySet) {
+                String temp;
+                Object obj = intent.getExtras().get(key);
+                if (obj != null) {
+                    String value = obj.toString();
+                    temp = "key = " + key + " || value = " + value;
+                } else {
+                    temp = "key = " + key + " || value = null";
+                }
+                Log.e(TAG, temp);
+                sb.append(temp);
+                sb.append("\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        sb.append("transType: " + intent.getIntExtra("transType", -99999) + "\n");
-        sb.append("packageName: " + intent.getStringExtra("packageName") + "\n");
-        sb.append("resultCode: " + intent.getIntExtra("resultCode", -99999) + "\n");
-        sb.append("errorCode: " + intent.getIntExtra("errorCode", -99999) + "\n");
-        sb.append("errorMsg: " + intent.getStringExtra("errorMsg") + "\n");
-        sb.append("model: " + intent.getStringExtra("model") + "\n");
-        sb.append("version: " + intent.getStringExtra("version") + "\n");
-        sb.append("terminalId: " + intent.getStringExtra("terminalId") + "\n");
-        sb.append("merchantName: " + intent.getStringExtra("merchantName") + "\n");
-        sb.append("transDate: " + intent.getStringExtra("transDate") + "\n");
-        sb.append("transTime: " + intent.getStringExtra("transTime") + "\n");
-        sb.append("operatorId: " + intent.getStringExtra("operatorId") + "\n");
-        sb.append("transId: " + intent.getStringExtra("transId") + "\n");
-        sb.append("amount: " + intent.getLongExtra("amount", -99999L) + "\n");
-        sb.append("voucherNo: " + intent.getStringExtra("voucherNo") + "\n");
-        sb.append("batchNo: " + intent.getStringExtra("batchNo") + "\n");
-        sb.append("referenceNo: " + intent.getStringExtra("referenceNo") + "\n");
-        sb.append("authNo: " + intent.getStringExtra("authNo") + "\n");
-        sb.append("qrOrderNo: " + intent.getStringExtra("qrOrderNo") + "\n");
-        sb.append("cardNo: " + intent.getStringExtra("cardNo") + "\n");
-        sb.append("cardType: " + intent.getStringExtra("cardType") + "\n");
-        sb.append("accountType: " + intent.getStringExtra("accountType") + "\n");
-        sb.append("issue: " + intent.getStringExtra("issue") + "\n");
-        sb.append("acquire: " + intent.getStringExtra("acquire") + "\n");
-        sb.append("answerCode: " + intent.getStringExtra("answerCode") + "\n");
-        sb.append("transactionType: " + intent.getIntExtra("transactionType", -99999) + "\n");
-        sb.append("transactionPlatform: " + intent.getIntExtra("transactionPlatform", -99999) + "\n");
-        sb.append("qrCodeScanModel: " + intent.getIntExtra("qrCodeScanModel", -99999) + "\n");
-        sb.append("qrCodeTransactionState: " + intent.getIntExtra("qrCodeTransactionState", -99999) + "\n");
-
-        sb.append("expDate: " + intent.getStringExtra("expDate") + "\n");
-        sb.append("entryMode: " + intent.getStringExtra("entryMode") + "\n");
-        sb.append("cardHolderName: " + intent.getStringExtra("cardHolderName") + "\n");
-
-        sb.append("\n 商户信息相关数据 \n");
-        sb.append("merchantNameEn: " + intent.getStringExtra("merchantNameEn") + "\n");
-
-        sb.append("\n 余额相关数据 \n");
-        sb.append("balance: " + intent.getLongExtra("balance", -99999) + "\n");
-
-        sb.append("\n 结算相关数据 \n");
-        sb.append("transNum: " + intent.getIntExtra("transNum", -99999) + "\n");
-        sb.append("totalAmount: " + intent.getLongExtra("totalAmount", -99999) + "\n");
-        sb.append("settleJson: " + intent.getStringExtra("settleJson") + "\n");
 
         String result = sb.toString();
         Log.e(TAG, "result: " + result);
 
         tvResult.setText(result);
-
     }
 
 
--- L3Demo/app/src/main/java/com/sm/l3/demo/ResultReceiver.java
+++ L3Demo/app/src/main/java/com/sm/l3/demo/ResultReceiver.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 public class ResultReceiver extends BroadcastReceiver {
 
     @Override
-    public void onReceive(final Context context, final Intent intent) {
+    public void onReceive(Context context, Intent intent) {
         intent.setClass(context, ResultActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
